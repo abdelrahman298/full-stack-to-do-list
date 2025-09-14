2f759c7cec39dcf9230edea4c58561d10bad0a8c
abdelrahman <abdelrahman298>
2025-09-14 23:04:11 +0300

    edit clerk protected route

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isProtectedRoute   = createRouteMatcher(["/"]);
 // const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  } 
+
+  if (isProtectedRoute(req)) await auth.protect()
+
 });
 
 export const config = {
